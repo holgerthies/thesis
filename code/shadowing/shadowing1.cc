9a9084673ae4d4ae1848f085c910b27d3abb093f
Holger Thies <info@holgerthies.com>
2014-12-22 20:49:01 +0100

    finding the shadow orbit with irram

--- code/shadowing/shadowing1.cc
+++ code/shadowing/shadowing1.cc
@@ -2,17 +2,30 @@
 #include <vector>
 #include <algorithm>
 
+#include <boost/multiprecision/mpfr.hpp>  // Defines the Backend type that wraps MPFR
+#include <boost/multiprecision/cpp_bin_float.hpp> 
 
-const float a=3.8;
-const float p0=0.4;
+namespace mp = boost::multiprecision;    
+const unsigned int cray_double_precision = 96;
+const unsigned int cray_float_precision = 49;
+typedef mp::number<mp::backends::cpp_bin_float<cray_float_precision, mp::backends::digit_base_2, void, boost::int16_t, -8192, 8191>, mp::et_off> cray_float;
+//typedef double cray_float;
+typedef mp::number<mp::cpp_bin_float<cray_double_precision, mp::digit_base_2>>  cray_double;
+
+
+const double a=3.8;
+const cray_double ad=3.8;
+const cray_double p0d=0.4;
+const double p0=0.4;
 using namespace iRRAM;
 using std::endl;
 using std::vector;
 using std::max;
 
 //  computes the logistic map for a dyadic number x
-float logistic_map(const float x){
-	return a*x*(1-x);
+cray_float logistic_map(const cray_float x){
+	cray_float ac = static_cast<cray_float>(ad);
+	return ac*x*(1-x);
 }
 
 //  computes the logistic map for a dyadic number x
@@ -35,9 +48,9 @@ REAL inverse_logistic_map(const REAL& x, bool left){
 // computes a pseudo orbit (noisy orbit).
 // Applies the logistic map N times. 
 // The output is a vector of the result after each step
-vector<float> pseudo_orbit(const int N){
-	vector<float> ans(N+1);
-	ans[0] = p0;
+vector<cray_float> pseudo_orbit(const int N){
+	vector<cray_float> ans(N+1);
+	ans[0] = static_cast<cray_float>(p0d);
 	for(int i=1; i<=N; i++){
 		ans[i] = logistic_map(ans[i-1]);
 	}
@@ -46,53 +59,35 @@ vector<float> pseudo_orbit(const int N){
 
 
 void compute(){
-	const int N=1000000;
-	// max distance between pseudo orbit and closest of the real orbits currently computed
-	REAL max_dist = 1000.0;
-	vector<float> orbit = pseudo_orbit(N);
-	REAL grid_size = power(REAL(2), -45);
-	REAL left = REAL(orbit[N])-grid_size;
-	REAL best_p = orbit[0];
-	int n=2;
-	while(!bound(max_dist, -18)){
-		cout << n << endl;
-		for(int i=0; i<n; i++){
-			// max distance between real and pseudo orbit for current start point
-			REAL start_point_dist=0.0; 
-			// start point
-			REAL p = left+i*grid_size;
-			for(int j=N-1;j>=0; j--){
-				bool left= (orbit[j] <= 0.5);
-				p = inverse_logistic_map(p, left);
-				REAL dist = abs(p-REAL(orbit[j]));
-				// multivalued version of dist > start_point_dist
-				if(positive(dist-start_point_dist,-100))
-					start_point_dist = dist;
-				if(!bound(dist, -15)) 
-					break;
-			}
-			// multivalued version of max_dist > start_point_dist
-			if(positive(max_dist-start_point_dist,-100)){
-				best_p = p; // this is now p0
-				max_dist = start_point_dist;
-			}
+	const int N=10000000;
+	vector<cray_float> orbit = pseudo_orbit(N);
+	// distance between pseudo_orbit and exact orbit
+	REAL pi = p0; 
+	REAL max_dist = 0;
+	for(int i=1; i<=N;i++){
+		REAL pi = logistic_map(REAL(p0));
+		REAL dist =abs(pi-REAL(float(orbit[i])));
+		if(positive(dist-max_dist,-100)){
+			max_dist = dist;
 		}
-		cout<<best_p<<" "<<orbit[0]<<endl;
-		grid_size /= 2;
-		n *= 2;
-		rshow(max_dist, 500);
-		cout << endl;
 	}
-	cout<<best_p<<endl;
-	REAL x=best_p;
-	REAL md = 0.0;
-	for(int i=0;i<=N;i++){
-		REAL dist = abs(x-REAL(orbit[i]));
-		if(positive(dist-md,-100))
-			md = dist;
-		x = logistic_map(x);
+	cout << "max distance between real orbit ans pseudo orbit : "<<max_dist<<endl;
+	// max distance between pseudo orbit and closest of the real orbits currently computed
+	max_dist = 0;
+	REAL p = double(orbit[N]);
+	//std::cout <<orbit[N]<< endl;
+	cout <<p <<endl;
+	for(int i=N-1;i>=0; i--){
+		bool left= (double(orbit[i]) <= 0.5);
+		if(double(orbit[i]) == 0.5)
+			std::cout << i<<" "<<orbit[i]<<endl;
+		p = inverse_logistic_map(p, left);
+		REAL dist = abs(p-REAL(double(orbit[i])));
+		if(positive(dist-max_dist,-10))
+			max_dist = dist;
 	}
-	cout<<md<<endl;
+	cout << max_dist <<endl;
+
 	/*
 	best_p=orbit[0];
 	grid_size = power(REAL(2), -20);
