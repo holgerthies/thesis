f0fc3fbfd27617f1eb8581b7417db6dbd85e5349
Holger Thies <info@holgerthies.com>
2015-03-06 19:45:21 +0100

    added code example

---
+++ code/analytic_example.cc
@@ -0,0 +1,25 @@
+#include "iRRAM.h"
+#include "Functions/Analytic/BA_ANA.h"
+#include "Functions/Analytic/ANA_RECT.h"
+
+using namespace iRRAM;
+
+REAL factorial(const int n){
+	if (n==0)
+		return REAL(1);
+	return factorial(n-1)*REAL(n);
+}
+
+REAL sinseries(const int n){
+	if (n%2 == 0) return 0;
+	if (n % 4 == 3) return -1/factorial(n); 
+	return 1/factorial(n); 
+}
+
+void compute() {
+	POWERSERIES<REAL> ps_sin(sinseries); 
+	BA_ANA<REAL> sinus(ps_sin, 2,2);
+	sinus.differentiate(2);
+	rwrite(sinus(0.2), 20);
+	iRRAM::cout << endl;
+}
